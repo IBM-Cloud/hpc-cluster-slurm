8b45f6ce3690f6be862324f0edad72a71125a986
Sagar BK <Sagar.BK1@ibm.com>
2024-05-21 22:07:53 +0530

    latest slurm changes from private repo

--- scripts/user_data_storage.sh
+++ scripts/user_data_storage.sh
@@ -3,29 +3,24 @@
 # Licensed under the Apache License v2.0
 ###################################################
 
+logfile=/tmp/user_data.log
 SHARED_TOP=/data
 
 env
 
-#Update management node host name based on internal IP address
-# privateIP=$(ip addr show eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
-# hostName=ibm-gen2host-${privateIP//./-}
+# Update hostname based on internal IP address
 hostname=${cluster_prefix}-storage
-hostnamectl set-hostname ${hostName}
+hostnamectl set-hostname ${hostname}
 
-# NOTE: On ibm gen2, the default DNS server do not have reverse hostname/IP resolution.
-# 1) put the management node hostname and ip into lsf hosts.
-# 2) put all possible VMs' hostname and ip into lsf hosts.
-python -c "import ipaddress; print('\n'.join([str(ip) + ' ibm-gen2host-' + str(ip).replace('.', '-') for ip in ipaddress.IPv4Network(bytearray('${rc_cidr_block}'))]))" >> /etc/hosts
-
+# Install and Configure NFS mount
 yum install -y nfs-utils
 found=0
 while [ $found -eq 0 ]; do
-    for vdx in `lsblk -d -n --output NAME`; do
+    for vdx in $(lsblk -d -n --output NAME); do
         desc=$(file -s /dev/$vdx | grep ': data$' | cut -d : -f1)
         if [ "$desc" != "" ]; then
             mkfs -t xfs $desc
-            uuid=`blkid -s UUID -o value $desc`
+            uuid=$(blkid -s UUID -o value $desc)
             echo "UUID=$uuid $SHARED_TOP xfs defaults,noatime 0 0" >> /etc/fstab
             mkdir -p $SHARED_TOP
             mount $SHARED_TOP
@@ -41,51 +36,41 @@ while [ $found -eq 0 ]; do
     sleep 1s
 done
 
-echo "$SHARED_TOP      ${rc_cidr_block}(rw,no_root_squash)" > /etc/exports.d/export-nfs.exports
+echo "$SHARED_TOP ${rc_cidr_block}(rw,no_root_squash)" > /etc/exports.d/export-nfs.exports
 exportfs -ar
 
-#Adjust the number of threads for the NFS daemon
-#NOTE: This would only work for RH7/CentOS7.
-#      On RH8, need to adjust /etc/nfs.conf instead
-ncpus=$( nproc )
-#default is 8 threads
-nthreads=8
-
-if [ "$ncpus" -gt "$nthreads" ]; then
-  echo "Adjust the thread number for NFS from $nthreads to $ncpus"
-  sed -i "s/^# *RPCNFSDCOUNT.*/RPCNFSDCOUNT=$ncpus/g" /etc/sysconfig/nfs
-fi
-
+# Restart and enable NFS service
 systemctl start nfs-server
 systemctl enable nfs-server
 
-echo END `date '+%Y-%m-%d %H:%M:%S'`
-
-
-copyFile (){
-    #This function will check for file existence, if not present, wait and recheck for file presence than copies.
-  fileSource=$1  #source file path
-  fileDestination=$2  #distination file path
-  delay=$3  #time delay between retries in seconds
-  retry=$4  #retry count
-  logfile=$5  #logfile path
+# Function to check for file existence and copy it
+copyFile() {
+  fileSource=$1  # source file path
+  fileDestination=$2  # destination file path
+  delay=$3  # time delay between retries in seconds
+  retry=$4  # retry count
+  logfile=$5  # logfile path
   retryCount=0
   echo "$fileSource file copy wait count : $retryCount" >> $logfile
-  while [[ ! -f $fileSource ]] && [[ $retryCount -le $retry ]]
-  do
+  while [[ ! -f $fileSource ]] && [[ $retryCount -le $retry ]]; do
     sleep $delay
     retryCount=$((retryCount + 1))
     echo "$fileSource file copy wait count : $retryCount" >> $logfile
   done
-  if cp $fileSource $fileDestination >> $logfile; then
+  if cp $fileSource $fileDestination >> $logfile 2>&1; then
     echo "$fileDestination copied" >> $logfile
   else
     echo "$fileDestination copy failed" >> $logfile
   fi
 }
-# copy the ssh key from nfs to storage
+
+# Copy the SSH key from NFS to storage
 copyFile $SHARED_TOP/ssh/id_rsa /root/.ssh/id_rsa 10 30 $logfile
-# copy the hosts list from nfs to storage
+
+# Copy the hosts list from NFS to storage
 copyFile $SHARED_TOP/hosts /etc/hosts 10 30 $logfile
+
 sleep 20
 cat $SHARED_TOP/ssh/authorized_keys >> /root/.ssh/authorized_keys
+
+echo "END $(date '+%Y-%m-%d %H:%M:%S')"
